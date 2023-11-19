d6612a2e7028700a695d3fbba63db4f0c7200e18
melissavelasquezgreene <melissa0191@outlook.es>
2023-11-18 23:38:04 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity2 = 'go to the office') {
+    return `This Monday, I will ${activity2}.`;
+}
+
+
+function wrapAdjective(message = "a hard worker") {
+    const innerAdj = function (special = "*") {
+          return `You are ${message}${special}${message}!`;
+      }
+      return innerAdj
+  }
+
